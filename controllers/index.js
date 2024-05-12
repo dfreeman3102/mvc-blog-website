57e8b7f402acc06c8db4a42d0053ccb25f742b31
dfreeman3102 <davidfreeman3102@gmail.com>
2024-05-11 21:51:58 -0400

    added route function, changed and added file names

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const homeRoute = require('./home-route');
+const apiRoute = require('./api');
+
+router.use('/', homeRoute);
+router.use('/api', apiRoute);
+
+module.exports = router;
