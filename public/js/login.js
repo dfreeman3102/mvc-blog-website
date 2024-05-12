c51b1b1293855d5f3227f730bc252b263223cf4b
dfreeman3102 <davidfreeman3102@gmail.com>
2024-05-12 17:44:45 -0400

    added login functionality

---
+++ public/js/login.js
@@ -0,0 +1,46 @@
+const loginHandler = async (event) => {
+  event.preventDefault();
+  //gets email and password from login form
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+const signupHandler = async (event) => {
+  event.preventDefault();
+  //gets name, email, and password from signup form
+  const name = document.querySelector("#name-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (name && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document.querySelector(".login-form").addEventListener("submit", loginHandler);
+
+document.querySelector(".signup-form").addEventListener("submit", signupHandler);
