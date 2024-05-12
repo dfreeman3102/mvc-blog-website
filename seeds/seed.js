3b86662a6307d16ff82333dd1a4fee431a443991
dfreeman3102 <davidfreeman3102@gmail.com>
2024-05-11 21:26:12 -0400

    added seeding function and added npm shortcut

---
+++ seeds/seed.js
@@ -0,0 +1,26 @@
+//adding sequelize and adding access to the models folder
+const sequelize = require('sequelize');
+const {User, Blog} = require('../models');
+//adding both .json folders to get the seed data from
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+//adding function to seed the database
+const seedDatabase = async () => {
+    await sequelize.sync({force: true});
+    //adding userData
+    const users = await User.bulkCreate(userData, {
+        individualHooks:true,
+        returning:true,
+    });
+    //adding blogData
+    for (const blog of blogData) {
+        await Blog.create({
+            ...blog,
+            user_id: users[Math.floor(Math.random() * users.length)].id,
+        });
+    }
+
+    process.exit(0);
+}
+
+seedDatabase();
