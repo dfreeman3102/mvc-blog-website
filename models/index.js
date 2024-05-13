10ad8a3c96e61af1473adc0486de8843e5325405
dfreeman3102 <davidfreeman3102@gmail.com>
2024-05-12 22:29:10 -0400

    added relations with comments model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./user.js');
 const Blog = require('./blogs.js');
+const Comment = require('./comments.js');
 
 User.hasMany(Blog, {
     foreignKey:'user_id',
@@ -10,4 +11,13 @@ Blog.belongsTo(User, {
     foreignKey:'user_id'
 });
 
-module.exports = { User , Blog };
+User.hasMany(Comment, {
+    foreignKey:'user_id',
+    onDelete:"CASCADE"
+});
+
+Comment.belongsTo(User, {
+    foreignKey:'user_id'
+});
+
+module.exports = { User , Blog , Comment};
