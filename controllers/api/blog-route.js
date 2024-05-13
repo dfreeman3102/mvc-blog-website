304b9e4a8415d6348118935875032b2fb5454b17
dfreeman3102 <davidfreeman3102@gmail.com>
2024-05-12 22:44:09 -0400

    added post comment route

--- controllers/api/blog-route.js
+++ controllers/api/blog-route.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Blog } = require('../../models');
+const { Blog, Comment } = require('../../models');
 const authorize = require('../../utils/auth.js');
 
 router.post('/', authorize, async (req, res) => {
@@ -11,6 +11,22 @@ router.post('/', authorize, async (req, res) => {
         res.status(200).json(newBlog);
     } catch (err) {
         res.status(400).json(err);
+    }
+});
+
+router.post('/comment', authorize, async (req, res) => {
+    try{
+        //get comment data from req.body
+        const {user_comments, blog_id} = req.body;
+        //create new comment in db
+        const newComment = await Comment.create({
+            user_comments,
+            blog_id,
+            user_id
+        });
+        res.status(200).json(newComment);
+    } catch (err) {
+        res.status(500).json({ message: 'Failed to create comment' });
     }
 });
 
